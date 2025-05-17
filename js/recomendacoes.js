abcae07359697e3c82d72fa294f18850d6ff673f
Vinicius Fernandes <viniciusfernandes126@gmail.com>
2025-05-17 19:41:17 -0300

    desenvolve lista de recomendações personalizavel com script e js

---
+++ js/recomendacoes.js
@@ -0,0 +1,24 @@
+const listaDeRecomendacoes = [
+    "Todos",
+    "Filmes",
+    "Séries",
+    "Desenhos",
+    "Desenvolvimento Web",
+    "Sistema de Informação",
+    "Videos curtos",
+    "Ao Vivo"
+];
+
+const recomendacoesElement = document.getElementById('recomendacoes');
+
+function gerarElementos() {
+    listaDeRecomendacoes.map((recomendacao) => {
+        let pTagElement = document.createElement('p');
+
+        pTagElement.innerText = recomendacao;
+
+        recomendacoesElement.appendChild(pTagElement);
+    });
+}
+
+gerarElementos();
